ade5a6a9fd4ac3de3521a176bef4017eb3c91f72
BrunoDMO <brunonaw41@gmail.com>
2023-11-27 16:30:15 -0300

    state of image and button change

--- 3411-praticando-logica-projeto_inicial/alugames/js/app.js
+++ 3411-praticando-logica-projeto_inicial/alugames/js/app.js
@@ -1,13 +1,16 @@
 function alterarStatus(idGame) {
     const game = document.querySelector(`#game-${idGame}`); // get the game selected
-    const situacao = game.lastElementChild; 
+    const gameImage = game.querySelector('.dashboard__item__img');
+    const situacao = game.querySelector('.dashboard__item__button');
 
     //check the state of the game and change
     if (situacao.innerHTML == 'Alugar') {
         situacao.innerHTML = 'Devolver';
-        situacao.setAttribute('class', 'dashboard__item__button dashboard__item__button--return' );
+        situacao.classList.add('dashboard__item__button--return');
+        gameImage.classList.add('dashboard__item__img--rented');
     } else {
         situacao.innerHTML = 'Alugar';
-        situacao.setAttribute('Class', 'dashboard__item__button');
+        situacao.classList.remove('dashboard__item__button--return');
+        gameImage.classList.remove('dashboard__item__img--rented');
     }
 }
